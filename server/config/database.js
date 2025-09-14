c6711c298eb5bca4cc89c02d7d698f9661858aa8
aditya <adityamistri19@gmail.com>
2025-09-14 16:07:07 +0530

    Phase 3 : DB config update

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 dotenv.config();
 
@@ -13,35 +15,42 @@ const sequelize = connectionString
       dialectOptions: {
         ssl: {
           require: true,
-          rejectUnauthorized: false, // Needed for Supabase
+          rejectUnauthorized: false,
         },
+        connectTimeout: 30000, // optional, increases timeout
+        family: 4, // 👈 force IPv4
       },
       logging: false,
     })
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: "postgres",
-      logging: !isProd ? console.log : false,
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-      define: {
-        timestamps: true,
-        underscored: false,
-        freezeTableName: true,
-      },
-      dialectOptions: isProd
-        ? {
-            ssl: {
-              require: true,
-              rejectUnauthorized: false,
-            },
-          }
-        : {},
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        logging: !isProd ? console.log : false,
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000,
+        },
+        define: {
+          timestamps: true,
+          underscored: false,
+          freezeTableName: true,
+        },
+        dialectOptions: isProd
+          ? {
+              ssl: {
+                require: true,
+                rejectUnauthorized: false,
+              },
+            }
+          : {},
+      }
+    );
 
 export default sequelize;
